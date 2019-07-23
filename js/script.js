e520e4e0d11d82c3f12e457a4112c181f1c4df85
Jeremy <xdetroit@live.com>
2019-07-22 20:41:49 -0400

    Finished js lab1

---
+++ js/script.js
@@ -0,0 +1,45 @@
+let name = "Jeremy Taylor";
+let age = "28";
+let birthday = "December 7th";
+let detroitGC = true;
+let lifeEvents = [
+  "Born in Detroit, Michigan",
+  "I have 2 Kids",
+  "I coached Soccer the YMCA in Southgate",
+  "I started working at the age of 13"
+];
+
+if (detroitGC === true) {
+  console.log(`
+    My name is ${name} and I am a student at Grand Circus in Detroit, Michigan.
+    I am currently ${age} years old and my birthday is on ${birthday}.`);
+} else {
+  console.log(`
+    My name is ${name} and I am a student at Grand Circus in Grand Rapids,
+    Michigan. I am currently ${age} years old and my birthday is on ${birthday}.`);
+}
+
+let i = 0;
+while (i < lifeEvents.length) {
+  console.log(lifeEvents[i]);
+  i++;
+}
+
+let randomNumber = Math.floor(Math.random() * 10);
+console.log(randomNumber);
+
+let counter = 0;
+
+while (true) {
+  if (randomNumber != 5) {
+    console.log(`${randomNumber} !== 5`);
+    randomNumber = Math.floor(Math.random() * 10 + 1);
+    counter++;
+  } else {
+    counter++;
+    console.log(`
+      5 === 5. It took ${counter}
+      iterations to randomly generate the number 5.`);
+    break;
+  }
+}
